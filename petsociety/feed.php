b648c9540bb47aefcc06a1168fab44d29f56d302
evelinesofie <50612469+evelinesofie@users.noreply.github.com>
2021-04-21 13:45:59 +0200

    Update feed.php

--- petsociety/feed.php
+++ petsociety/feed.php
@@ -1,56 +1,73 @@
 <?php include 'partials/header.php'; ?>
+
 <?php if ($_SESSION) { ?>
     <main>
         <form method="post">
             <!-- Image upload is missing -->
-            <input name="text" type="text"><br>
+            <textarea name="text" type="text"></textarea><br>
             <input type="submit" name="postbtn" value="Post">
         </form>
         
     </main>
-<?php } else {
-    header("Location: login.php");
-} ?>
+    <?php } else {
+        header("Location: login.php");
+    } ?>
 
-<?php
+    <?php
 
-    $query = "SELECT post.text, post.timestamp, user.firstname
-    FROM post
-    INNER JOIN user ON post.userID=user.userID";
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        //Add new post
+        if (isset($_POST["postbtn"])) {
+            if (!empty($_POST["text"])) {
 
-    while ($row = $result->fetch_assoc()) {
-        echo "<p>" . $row['text'] . "</p>";
-        echo "<p>Posted by " . $row['firstname'] . "</p>";
-        echo "<p>" . $row['timestamp'] . "</p>";
-    }
-    
-    if (isset($_POST["postbtn"])) {
+                $text = $_POST['text'];
 
-        if (!empty($_POST["text"])) {
+                $text = htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 
-            //Get form data
-            $text = $_POST['text'];
-
-            //Sanitize data
-            $text = htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
-
-            //Add post in db
-            $query = "INSERT INTO post (text, userID) VALUES (?, ?)";
-            $stmt = $db->prepare($query);
-            $stmt->bind_param("si", $text, $_SESSION['userId']);
-            if ($stmt->execute()) {
-                header("Location: feed.php");
+                $query = "INSERT INTO post (text, userID) VALUES (?, ?)";
+                $stmt = $db->prepare($query);
+                $stmt->bind_param("si", $text, $_SESSION['userId']);
+                
+                if ($stmt->execute()) {
+                    header("Location: feed.php");
+                }
+                
+                $stmt->close();
+                
+            } else {
+                echo "Please write something to publish the post.";
             }
-
-            $stmt->close();
-        } else {
-            echo "Please write something to create the post.";
         }
-    }
 
-?>
+        //Fetch all posts & comments and display
+        $query = "SELECT post.postID AS post_postid, post.text AS post_text, post.timestamp AS post_timestamp, user.userID AS user_userid, user.username AS user_username, comment.postID AS comment_postid, comment.userID AS comment_userid, comment.text AS comment_text, comment.timestamp AS comment_timestamp
+        FROM post
+        INNER JOIN user ON post.userID = user.userID
+        LEFT JOIN comment ON post.postID = comment.postID
+        ORDER BY post.postID DESC";
+
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<p>" . $row['post_text'] . "</p>";
+            echo "<p>Posted by " . $row['user_username'] . "</p>";
+            echo "<p>" . $row['post_timestamp'] . "</p>";
+            echo "<form method='post'><textarea name='text' type='text'></textarea><br>";
+            echo "<button name='commentbtn" . $row['post_postID'] . "'>Comment</button></form>";
+            echo "<p>" . $row['comment_text'] . "</p>";
+        }
+
+        $query = "SELECT comment.commentID, user.username
+        FROM comment
+        INNER JOIN user ON comment.userID=user.userID";
+                $stmt = $db->prepare($query);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                while ($row = $result->fetch_assoc()) {
+                    echo $row['username'];
+                }
+
+    ?>
 
 <?php include 'partials/footer.php'; ?>

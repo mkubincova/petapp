0ba15a87846d282300aab8e9c8948e31799b2e1a
Magdalena Kubincova <sleepydesertfox@tutanota.com>
2021-06-21 11:44:56 +0200

    Homepage redesign

--- petsociety/home.php
+++ petsociety/home.php
@@ -1,12 +1,14 @@
 <?php include 'partials/header.php'; ?>
 
 <main>
+    <img src="img/home.png" alt="home-bkg" id="home-img">
     <div class="home-container">
         <h1>Welcome to Pet Society</h1>
-        <h3>The ultimate platform for pet owners</h3>   
-        <a href="login.php"><button>Log in</button></a><br>
+        <h3>The ultimate platform for pet owners</h3>
+        <a href="login.php"><button>Log in</button></a>
         <a href="register.php"><button>Register</button></a>
-    </div> 
+    </div>
+
 </main>
 
 <?php include 'partials/footer.php'; ?>
